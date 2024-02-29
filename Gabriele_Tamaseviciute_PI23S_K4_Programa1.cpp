0d0755001fb02760eb955ba890eb88b82abdad21
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-02-29 11:33:21 +0200

    Add files via upload

---
+++ Gabriele_Tamaseviciute_PI23S_K4_Programa1.cpp
@@ -0,0 +1,107 @@
+/* Ivesti sveika skaiciu N. Ivesti N sveiku skaiciu a1, a2, ... aN. Sekoje a1, a2, ... aN visus
+narius, mazesnius uz didziausia sekos nari, padvigubinti. Isvesti gauta seka.
+
+
+Visuose uzdaviniuose reikia naudoti vienkrypcius sarasus bei funkcijas, kuriose turi buti
+realizuoti prasomi veiksmai. Taip turi buti realizuotos ivedimo bei spausdinimo funkcijos.*/
+
+#include <iostream>
+
+using namespace std;
+
+struct Mazgas {
+	int duomuo;
+	Mazgas* kitas;
+};
+
+
+Mazgas* SukurtiMazga(int duomuo) {
+	Mazgas* newMazgas = new Mazgas;
+	newMazgas->duomuo = duomuo;
+	newMazgas->kitas = nullptr; 
+	return newMazgas;
+}
+
+void PridetiElementa(Mazgas*& pradzia, int duomuo) {
+	Mazgas* naujasMazgas = SukurtiMazga(duomuo);
+	if (pradzia == nullptr) {
+		pradzia = naujasMazgas; // Sàraðas yra tuðèias, todël naujas mazgas tampa pradþia
+	}
+	else {
+		Mazgas* dabartinis = pradzia;
+		while (dabartinis->kitas != nullptr) {
+			dabartinis = dabartinis->kitas; // Eina per sàraðà iki paskutinio elemento
+		}
+		dabartinis->kitas = naujasMazgas; // Paskutiniojo mazgo 'kitas' nurodo á naujai sukurtà mazgà
+	}
+	naujasMazgas->kitas = nullptr; // Uþtikrinama, kad naujojo mazgo 'kitas' bûtø nullptr
+}
+
+
+
+void apdorojimas(Mazgas* pradzia) {
+	if (!pradzia) return;
+
+	int didziausias = pradzia->duomuo;
+	Mazgas* dabartinis = pradzia;
+
+	// Rasti didþiausià skaièiø
+	while (dabartinis != nullptr) {
+		if (dabartinis->duomuo > didziausias) {
+			didziausias = dabartinis->duomuo;
+		}
+		dabartinis = dabartinis->kitas;
+	}
+
+	// Padvigubinti maþesnius skaièius nei didþiausias
+	dabartinis = pradzia;
+	while (dabartinis != nullptr) {
+		if (dabartinis->duomuo < didziausias) {
+			dabartinis->duomuo *= 2;
+		}
+		dabartinis = dabartinis->kitas;
+	}
+}
+
+void RodytiVisaSarasa(Mazgas* pradzia) {
+	if (pradzia == nullptr) {
+		cout << "Sarasas yra tuscias." << endl;
+		return;
+	}
+
+	Mazgas* dabartinis = pradzia;
+	while (dabartinis != nullptr) {  // Tæsiamas ciklas, kol nepasiekiamas sàraðo pabaigos NULL
+		cout << dabartinis->duomuo << " ";
+		dabartinis = dabartinis->kitas;
+	}
+	cout << endl;
+}
+
+
+int main() {
+	Mazgas* pradzia = nullptr;
+	int N;
+	int num;
+
+	cout << "Iveskite sveikuju skaiciu kieki N: ";
+	cin >> N;
+
+	cout << "Iveskite " << N << " sveikuosius skaicius:" << endl;
+	for (int i = 0; i < N; i++) {
+		cout << "Iveskite " << i + 1 << "-aji skaiciu: "; 
+		cin >> num;
+		PridetiElementa(pradzia, num);
+	}
+
+	cout << "Sarasas dabar atrodo taip: ";
+	RodytiVisaSarasa(pradzia);
+
+	apdorojimas(pradzia);
+
+
+	cout << "\nModifikuotas sarasas: ";
+	RodytiVisaSarasa(pradzia);
+
+
+	return 0;
+}
